e2924f03c363f49e174c0205deeb73f6dc2dccf6
jane4332 <137068717+jane4332@users.noreply.github.com>
2025-01-27 02:59:00 +0900

    [Bronze II] Title: 벌집, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 1;
+        int range =2;
+      
+        if(n == 1){
+            System.out.println(1);
+        }else{
+
+            while(range<=n){
+                range = range +(cnt*6);
+                cnt++;
+            }
+            System.out.println(cnt);
+        }
+       
+    }
+
+}
